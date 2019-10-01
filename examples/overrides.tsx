8f90908164c5669e90df56137234002b1e4d1458
shuhei <s.tagawa1009@gmail.com>
2019-10-02 02:45:35 +0900

    feat: update overrides sample component

--- examples/overrides.tsx
+++ examples/overrides.tsx
@@ -4,21 +4,54 @@ import { ReactDatePicks } from '../src'
 import '../assets/index.less'
 import moment from 'moment'
 
+const ComponentsOptions = [
+  'Root',
+  'YearMonthHeader',
+  'YearMonthBody',
+  'MonthOptions',
+  'YearMonthButton',
+  'YearOptions',
+  'DateField',
+  'YearMonthOption',
+  'SelectButton'
+]
+
 const Test: React.FC = () => {
   const [value, setValue] = useState(moment(new Date(), 'YYYY M').format('YYYY M'))
+  const [selectValue, setSelectValue] = useState('Root')
   const handleChange = (value: string) => {
     setValue(value)
+  }
+  const updateOperator = value => {
+    setSelectValue(value)
   }
   return (
     <div>
       <h2>Component Override</h2>
+      <div>
+        <select
+          value={selectValue}
+          onChange={e => {
+            updateOperator(e.target.value)
+          }}
+        >
+          {ComponentsOptions.map(data => {
+            return (
+              <option key={data} value={data}>
+                {data}
+              </option>
+            )
+          })}
+        </select>
+      </div>
+
       <ReactDatePicks
         value={value}
         handleChange={handleChange}
         placeholder="year month"
         overrides={{
-          Root: {
-            style: { width: '100%' }
+          [selectValue]: {
+            style: { backgroundColor: '#F7BFA5' }
           }
         }}
       />
